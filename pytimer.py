99a7a6c4a15d01d1d6503d8cdd0c4ffc2159cc10
homoluctus <w.slife18sy@gmail.com>
2018-06-27 14:36:48 +0900

    Change from function base to class base

--- pytimer.py
+++ pytimer.py
@@ -2,83 +2,105 @@ import sys
 import time
 import argparse
 import subprocess
+from random import randint
 from time import localtime, strftime
 from pydub import AudioSegment
 
 def parseArgs():
     parser = argparse.ArgumentParser(
-            prog='pytimer',
-            conflict_handler='resolve',
-            description='Alert when it comes to a specific time',
-        )
+        prog='pytimer',
+        conflict_handler='resolve',
+        description='Alert when it comes to a specific time',
+    )
 
     parser.add_argument(
-            'hour',
-            type=int,
-            nargs='?',
-            metavar='H',
-            help='Notify in H hours',
-        )
+        'hour',
+        type=int,
+        nargs='?',
+        metavar='H',
+        help='Notify in H hours',
+    )
 
     parser.add_argument(
-            'minute',
-            type=int,
-            nargs='?',
-            metavar='M',
-            help='Notify in M minutes',
-        )
+        'minute',
+        type=int,
+        nargs='?',
+        metavar='M',
+        help='Notify in M minutes',
+    )
 
     parser.add_argument(
-            'second',
-            type=int,
-            nargs='?',
-            metavar='S',
-            help='Notify in S seconds',
-        )
+        'second',
+        type=int,
+        nargs='?',
+        metavar='S',
+        help='Notify in S seconds',
+    )
 
     parser.add_argument(
-            '-t',
-            '--time',
-            type=int,
-            nargs=2,
-            metavar='H M',
-            dest='time',
-            help='Specify local time and Notify when it comes to specific time',
-        )
+        '-t',
+        '--time',
+        type=int,
+        nargs=2,
+        metavar='H M',
+        dest='time',
+        help='Specify local time and Notify when it comes to specific time',
+    )
 
     return vars(parser.parse_args())
 
-def countdown(hour, min, sec):
-    # set the unit to seconds
-    count = hour*360 + min*60 + sec
-    time.sleep(count)
+class Pytimer:
+    def __init__(self, count):
+        if count['time'] is None:
+            self.hour   = count['hour']
+            self.min    = count['minute']
+            self.sec    = count['second']
+        else:
+            self.hour   = count['time'][0]
+            self.min    = count['time'][1]
 
-def timer(hour, min):
-    current_hour = int(strftime("%H", localtime()))
-    current_min = int(strftime("%M", localtime()))
-    current_sec = int(strftime("%S", localtime()))
+    def setTime(self):
+        current_hour    = int(strftime("%H", localtime()))
+        current_min     = int(strftime("%M", localtime()))
+        current_sec     = int(strftime("%S", localtime()))
 
-    rest_hour = hour - current_hour
-    rest_min = min - current_min - 1
-    rest_sec = 61 - current_sec
+        if self.hour < current_hour:
+            self.hour = 24 - (current_hour - self.hour)
+        else:
+            self.hour -= current_hour
 
-    if rest_hour < 0 or rest_min < 0:
-        sys.exit("Error : Invalid time")
+        if self.min < current_min:
+            self.min = 60 - (current_min - self.min)
+        elif self.min == current_min:
+            self.min = 0
+        else:
+            self.min -= current_min + 1
 
-    countdown(rest_hour, rest_min, rest_sec)
+        self.sec = 60 - current_sec
+        print(self.hour, self.min, self.sec)
 
-def alert():
-    # play music on background
-    subprocess.run("aplay ~/src/python/hobby/timer/music/Far_Away_Sting.wav &", shell=True)
+    def countdown(self):
+        # set the unit to seconds
+        count = self.hour * 360 + self.min * 60 + self.sec
+        time.sleep(count)
 
-def main():
-    count = parseArgs()
-    if count['time'] is None:
-        countdown(count['hour'], count['minute'], count['second'])
-        alert()
-    else:
-        timer(count['time'][0], count['time'][1])
-        alert()
+    def alert(self):
+        # play music on background
+        sounds = [
+            "Far_Away_Sting.wav",
+            "Eyes_on_You_Sting.wav",
+            "A_Long_Cold_Sting.wav",
+        ]
+
+        key = randint(0, len(sounds)-1)
+        cmd = "aplay ~/src/python/hobby/timer/music/{} &".format(sounds[key])
+
+        subprocess.run(cmd, shell=True)
 
 if __name__ == '__main__':
-    main()
+    count   = parseArgs()
+    pytimer = Pytimer(count)
+    if count['time'] is not None:
+        pytimer.setTime()
+    pytimer.countdown()
+    pytimer.alert()
